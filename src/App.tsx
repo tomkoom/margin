ea7046efd9b92ecdba52fb9f98e20879504c8852
tomkoom <tomash.sugint@gmail.com>
2024-04-06 20:13:03 +0300

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 // https://dev.to/shashannkbawa/deploying-vite-app-to-github-pages-3ane
 import { FC } from "react";
+import "./main.css";
 import "./App.css";
 import { Home } from "./pages/_index";
 
 const App: FC = (): JSX.Element => {
-	return (
-		<div className="app">
-			<Home />
-		</div>
-	);
+  const theme = "dark";
+
+  return (
+    <div className={theme}>
+      <Home />
+    </div>
+  );
 };
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,57 +2,62 @@ import { FC } from "react";
 import { styled } from "styled-components";
 
 const Home: FC = () => {
-	return (
-		<HomeStyled>
-			<Content>
-				<h1>Margin Studio</h1>
-				<Intro>
-					<p
-						className="
-        description"
-					>
-						Web3 development studio focused on crafting products on the Internet Computer network
-					</p>
-
-					<p>
-						Reach out on{" "}
-						<a href="https://t.me/tomkoom" target="_blank" rel="noreferrer noopener">
-							Telegram
-						</a>{" "}
-						or <a href="mailto:tomash.sugint@gmail.com">send email</a>
-					</p>
-				</Intro>
-			</Content>
-		</HomeStyled>
-	);
+  return (
+    <HomeStyled>
+      <Content>
+        <h1>Margin 🛠️</h1>
+        <Intro>
+          <p>
+            Web3 development studio focused on crafting products on the{" "}
+            <a
+              href="https://internetcomputer.org/"
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              Internet Computer network
+            </a>
+            <br />
+            Reach out on{" "}
+            <a
+              href="https://t.me/tomkoom"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Telegram
+            </a>{" "}
+            or <a href="mailto:tomash.sugint@gmail.com">send email</a>
+          </p>
+        </Intro>
+      </Content>
+    </HomeStyled>
+  );
 };
 
 const HomeStyled = styled.div`
-	text-align: center;
-	padding: 1rem 1rem 4rem 1rem;
+  text-align: left;
+  padding: 1rem 1rem 4rem 1rem;
 
-	position: fixed;
-	top: 50%;
-	left: 50%;
-	-webkit-transform: translate(-50%, -50%);
-	transform: translate(-50%, -50%);
+  /* center */
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
 `;
 
-const Content = styled.div``;
+const Content = styled.div`
+  max-width: 36rem;
+`;
 
 const Intro = styled.div`
-	margin-top: 1rem;
-	display: flex;
-	flex-direction: column;
-	gap: 0.5rem;
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
 
-	> p {
-		font-size: var(--fs5);
-	}
-
-	> p.description {
-		max-width: 32rem;
-	}
+  > p {
+    font-size: var(--fs4);
+  }
 `;
 
 export default Home;
